2dce6d10c9bf77f0b1b1dffe92d5b2f2a1897446
Shobana <your@email.com>
2026-03-20 03:37:16 -0700

    add jdt dev (ls, clean)

--- cmd/jdt/main.go
+++ cmd/jdt/main.go
@@ -1,91 +1,109 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+        "fmt"
+        "os"
+        "strconv"
 
-	"github.com/WAZULU503/Name-james-dev-tool/internal/ports"
-	"github.com/WAZULU503/Name-james-dev-tool/internal/ui"
+        "github.com/WAZULU503/Name-james-dev-tool/internal/ports"
+        "github.com/WAZULU503/Name-james-dev-tool/internal/ui"
 )
 
-const version = "v0.1.0"
-
 func main() {
+        if len(os.Args) < 2 {
+                printUsage()
+                return
+        }
 
-	if len(os.Args) < 2 {
-		printUsage()
-		return
-	}
+        cmd := os.Args[1]
 
-	if os.Args[1] == "version" {
-		fmt.Println("jdt", version)
-		return
-	}
+        if cmd == "dev" || cmd == "p" || cmd == "port" {
+                if len(os.Args) < 3 {
+                        printUsage()
+                        return
+                }
+        }
 
-	if len(os.Args) < 3 {
-		printUsage()
-		return
-	}
+        sub := ""
+        if len(os.Args) >= 3 {
+                sub = os.Args[2]
+        }
 
-	cmd := os.Args[1]
-	sub := os.Args[2]
+        switch cmd {
 
-	switch cmd {
+        case "dev":
+                switch sub {
+                case "ls":
+                        items, err := ports.GetPorts()
+                        if err != nil || len(items) == 0 {
+                                fmt.Println("No active ports.")
+                                return
+                        }
+                        ui.PrintTable(items)
 
-	case "p", "port":
+                case "clean":
+                        err := ports.FreePorts()
+                        if err != nil {
+                                fmt.Println(err)
+                                return
+                        }
 
-		switch sub {
+                        // VERIFY AFTER CLEAN
+                        items, err := ports.GetPorts()
+                        if err != nil || len(items) == 0 {
+                                fmt.Println("✔ environment cleared (verified)")
+                                return
+                        }
 
-		case "ls", "list":
+                        fmt.Println("✖ environment not fully cleared")
+                        ui.PrintTable(items)
 
-			items, err := ports.GetPorts()
-			if err != nil {
-				fmt.Println("Error:", err)
-				return
-			}
+                default:
+                        printUsage()
+                }
 
-			ui.PrintTable(items)
+        case "p", "port":
+                switch sub {
+                case "ls", "list":
+                        items, err := ports.GetPorts()
+                        if err != nil {
+                                fmt.Println("Error:", err)
+                                return
+                        }
+                        ui.PrintTable(items)
 
-		case "k", "kill":
+                case "k", "kill":
+                        if len(os.Args) < 4 {
+                                fmt.Println("Specify port")
+                                return
+                        }
+                        port, _ := strconv.Atoi(os.Args[3])
+                        err := ports.KillPort(port)
+                        if err != nil {
+                                fmt.Println(err)
+                        }
 
-			if len(os.Args) < 4 {
-				fmt.Println("Specify port")
-				return
-			}
+                case "f", "free":
+                        err := ports.FreePorts()
+                        if err != nil {
+                                fmt.Println(err)
+                        }
 
-			port, _ := strconv.Atoi(os.Args[3])
+                default:
+                        printUsage()
+                }
 
-			err := ports.KillPort(port)
-
-			if err != nil {
-				fmt.Println(err)
-			}
-
-		case "f", "free":
-
-			err := ports.FreePorts()
-
-			if err != nil {
-				fmt.Println(err)
-			}
-
-		default:
-			fmt.Println("Unknown port command")
-		}
-
-	default:
-		printUsage()
-	}
+        default:
+                printUsage()
+        }
 }
 
 func printUsage() {
-
-	fmt.Println("jdt (James Dev Tool)")
-	fmt.Println()
-	fmt.Println("Commands:")
-	fmt.Println("  jdt p ls        List active dev ports")
-	fmt.Println("  jdt p k <port>  Kill process on port")
-	fmt.Println("  jdt p f         Free all dev ports")
-	fmt.Println("  jdt version     Show version")
+        fmt.Println("jdt")
+        fmt.Println("Usage:")
+        fmt.Println("  jdt p ls       - List active ports")
+        fmt.Println("  jdt p k <port> - Kill port")
+        fmt.Println("  jdt p f        - Free common ports")
+        fmt.Println("  jdt dev ls     - List dev ports")
+        fmt.Println("  jdt dev clean  - Clean dev environment")
 }
